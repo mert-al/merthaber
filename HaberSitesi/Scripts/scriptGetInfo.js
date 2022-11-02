add7b2eda3553e75b9522fd2c8cb104d39c20649
miracezra <miracergenc@gmail.com>
2022-11-02 16:24:26 +0300

    mergleme 2 user browser bilgileri eklendi

--- HaberSitesi/Scripts/scriptGetInfo.js
+++ HaberSitesi/Scripts/scriptGetInfo.js
@@ -1,29 +1,57 @@
-﻿////$.ajax({
-////    method: 'POST',
-////    type: 'json',
-////    data: {
-////        name: "Süleyman Çetiner",
-////        phone: "555-555-55-55"
-////    },
-////    url: 'http://localhost:8000/test/',
-////    success: function (response) {
-////        console.log(response);
-////    },
-////    error: function (error) {
-////        console.log("hata ajax",error);
-////    }
-////});
-
+﻿
 
 let fullUrl = window.location.href;
 let pathname = window.location.pathname;
 let href = pathname.slice(1, fullUrl.length);
 let findCategory = "HomePage or Menus";
-let newsCategory ="home page or menus";
+let newsCategory = "home page or menus";
+let userBrowser = "";
 if (href !== null || href !== "") {
     findCategory = href.split("/")[0];
     newsCategory = findCategory.split("-")[0];
 }
+
+if ((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1) {
+    userBrowser = "Opera"
+}
+else if (navigator.userAgent.indexOf("Edg") != -1) {
+    userBrowser = "Edge"
+}
+else if (navigator.userAgent.indexOf("Chrome") != -1) {
+    userBrowser = "Chrome"
+}
+else if (navigator.userAgent.indexOf("Safari") != -1) {
+    userBrowser = "Safari"
+}
+else if (navigator.userAgent.indexOf("Firefox") != -1) {
+    userBrowser = "Firefox"
+}
+else if ((navigator.userAgent.indexOf("MSIE") != -1) || (!!document.documentMode == true)) //IF IE > 10
+{
+    userBrowser = "IE"
+}
+else {
+    userBrowser = "unknow"
+}
+
+
+/*
+ *gerekli datalar
+ * _id
+ * usertokenid
+ * country
+ * city
+ * signtime
+ * category = ekonomi +
+ * news = thy-bazi-yurt-disi-ucuslarini-durdurma-kararini-uzatti=1" +
+ * browser = chrome + 
+ * OS = windows 
+ * isMobile = false
+ * device = computer
+ * duration = 
+ * created at 
+ * 
+ * */
 
 
 
@@ -32,19 +60,34 @@ let postObj = {
     pathname: pathname,
     href: href,
     category: findCategory,
+    browser: userBrowser,
    
 }
-let post = JSON.stringify(postObj)
 
-const url = "http://localhost:8000/test/"
-let xhr = new XMLHttpRequest()
-
-xhr.open('POST', url, true)
-xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8')
-xhr.send(post);
-
-xhr.onload = function () {
-    if (xhr.status === 201) {
-        console.log("Post successfully created!")
+$.ajax({
+    method: 'POST',
+    type: 'json',
+    data: postObj,
+    url: 'http://localhost:8000/test/',
+    success: function (response) {
+        console.log(response);
+    },
+    error: function (error) {
+        console.log("hata ajax", error);
     }
-}
+});
+
+//let post = JSON.stringify(postObj)
+
+//const url = "http://localhost:8000/test/"
+//let xhr = new XMLHttpRequest()
+
+//xhr.open('POST', url, true)
+//xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8')
+//xhr.send(post);
+
+//xhr.onload = function () {
+//    if (xhr.status === 201) {
+//        console.log("Post successfully created!")
+//    }
+//}
